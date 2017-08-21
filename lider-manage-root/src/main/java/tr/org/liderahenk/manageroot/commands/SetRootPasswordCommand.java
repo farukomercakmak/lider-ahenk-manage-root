8c7fe5f46421715f81975f71cae88cd91991fe02
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-08-21 11:25:58 +0300

    db added

--- lider-manage-root/src/main/java/tr/org/liderahenk/manageroot/commands/SetRootPasswordCommand.java
+++ lider-manage-root/src/main/java/tr/org/liderahenk/manageroot/commands/SetRootPasswordCommand.java
@@ -1,23 +1,31 @@
 package tr.org.liderahenk.manageroot.commands;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import tr.org.liderahenk.lider.core.api.log.IOperationLogService;
+import tr.org.liderahenk.lider.core.api.messaging.enums.StatusCode;
 import tr.org.liderahenk.lider.core.api.persistence.IPluginDbService;
-import tr.org.liderahenk.lider.core.api.rest.requests.ITaskRequest;
-import tr.org.liderahenk.lider.core.api.plugin.IPluginInfo;
+import tr.org.liderahenk.lider.core.api.persistence.entities.ICommandExecutionResult;
 import tr.org.liderahenk.lider.core.api.plugin.ICommand;
+import tr.org.liderahenk.lider.core.api.plugin.IPluginInfo;
+import tr.org.liderahenk.lider.core.api.plugin.ITaskAwareCommand;
+import tr.org.liderahenk.lider.core.api.rest.requests.ITaskRequest;
 import tr.org.liderahenk.lider.core.api.service.ICommandContext;
 import tr.org.liderahenk.lider.core.api.service.ICommandResult;
 import tr.org.liderahenk.lider.core.api.service.ICommandResultFactory;
 import tr.org.liderahenk.lider.core.api.service.enums.CommandResultStatus;
+import tr.org.liderahenk.manageroot.entities.ManageRootEntity;
 
-public class SetRootPasswordCommand implements ICommand {
+public class SetRootPasswordCommand implements ICommand, ITaskAwareCommand {
 
 	private Logger logger = LoggerFactory.getLogger(SetRootPasswordCommand.class);
 	
@@ -28,28 +36,37 @@ public class SetRootPasswordCommand implements ICommand {
 
 	@Override
 	public ICommandResult execute(ICommandContext context) {
-		
-		// TODO Modify parameter map before sending it to agent(s).
-		ITaskRequest req = context.getRequest();
-		Map<String, Object> parameterMap = req.getParameterMap();
-		parameterMap.put("dummy-param", "dummy-param-value");
-		
-		logger.debug("Parameter map updated.");
-	//	pluginDbService.findByProperties(entityClass, propertiesMap, orders, maxResults)
-		
-		// TODO Modify entity objects related to plugin command via DB service
-		//Object entity = new Object();
-		//pluginDbService.save(entity);
-		logger.debug("Entity saved successfully.");
-		
-		// TODO Modify result map to provide additional parameters or info before sending it back to console.
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("dummy-param", "dummy-param-value");
-		
-		logger.debug("Executed command, returning result.");
-		ICommandResult commandResult = resultFactory.create(CommandResultStatus.OK, new ArrayList<String>(), this, resultMap);
 
-		return commandResult;
+		 ITaskRequest req = context.getRequest();
+			
+		 Map<String, Object> parameterMap = req.getParameterMap();
+		 
+		 ObjectMapper mapper = new ObjectMapper();
+		 mapper.configure(
+				    DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		 try {
+			 ManageRootEntity rootEntity = new ManageRootEntity();
+					
+					if(rootEntity.getId()==null){
+						rootEntity.setCreateDate(new Date());
+						rootEntity.setAgentDn(context.getRequest().getDnList().get(0));
+						rootEntity.setOwner(req.getOwner());
+						
+						pluginDbService.save(rootEntity);
+						parameterMap.put("rootEntity", rootEntity);
+						
+					}else{
+						rootEntity.setModifyDate(new Date());
+						pluginDbService.update(rootEntity);
+					}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return resultFactory.create(CommandResultStatus.ERROR, new ArrayList<String>(), this);
+		} 
+		return resultFactory.create(CommandResultStatus.OK, new ArrayList<String>(), this);
+		
 	}
 
 	@Override
@@ -91,6 +108,43 @@ public class SetRootPasswordCommand implements ICommand {
 
 	public void setPluginDbService(IPluginDbService pluginDbService) {
 		this.pluginDbService = pluginDbService;
+	}
+
+	@Override
+	public void onTaskUpdate(ICommandExecutionResult result) {
+
+		try {
+			
+			byte[] data = result.getResponseData();
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			
+			final Map<String, Object> responseData = mapper.readValue(data, 0, data.length,
+					new TypeReference<HashMap<String, Object>>() {
+					});
+			
+				ManageRootEntity entity = new ObjectMapper().readValue(mapper.writeValueAsString(responseData.get("rootEntity")),
+						new TypeReference<ManageRootEntity>() {
+				});
+				
+				if(entity!=null && entity.getId()!=null){
+					
+					if(result.getResponseCode()==StatusCode.TASK_ERROR){
+						entity.setState("ERROR");
+					}
+					else
+						entity.setState("OK");
+					
+					entity.setAgentId(result.getAgentId());
+					pluginDbService.update(entity);
+				}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	
 	}
 	
 }
